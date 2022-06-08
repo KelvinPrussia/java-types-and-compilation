996de9f6aeb00b5f7729bc9493f27a20e4ac6def
Kelvin <prussiakelvin@gmail.com>
2022-06-08 12:21:31 +0100

    First commit

---
+++ PriceDisplay.java
@@ -0,0 +1,7 @@
+class PriceDisplay{
+  public static void main(String[] args){
+    String item = "Coffee machine";
+    Double price = 15.99;
+    System.out.printf("The price of the %s is £%.2f \n", item, price);
+  }
+}
